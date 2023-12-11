b7b89a08c3c58771ac4f7f99fee46f77917fab54
Matt Seidel <mseidel819@gmail.com>
2023-12-11 09:57:40 -0600

    write all to 1 file

--- scrape.py
+++ scrape.py
@@ -8,16 +8,18 @@ categories = ['bread', "breakfast-treats", "desserts/cakes", "desserts/cookies",
 total = 0
 HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
 
+recipeData = []
+
 for category in categories:
     cleaned_filename = category.replace("/", "-")
     url = f'https://sallysbakingaddiction.com/category/{category}/'
     href_list = get_urls(url, HEADERS)
     json_data = create_json(href_list, cleaned_filename, HEADERS)
     total += len(json_data)
+    recipeData.extend(json_data)
+    print(f'json successfully compiled for {category}!')
 
-    with open(f'sallys-baking-addiction/{cleaned_filename}.json', "w") as json_file:
-        json.dump(json_data, json_file, indent=2)
+with open('sallys-baking-addiction/sallys-baking-addiction.json', "w") as json_file:
+    json.dump(recipeData, json_file, indent=2)
 
-    print(f'{category}.json successfully written!')
-
-print(f'Success! {total} recipes were downloaded')
+print(f'Success! {total} recipes were added to sallys-baking-addiction.json')
